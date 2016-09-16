2f4d73e8b7c7492e8d488311c80f6a4995c7c8e7
Bartosz Szymanski <bartosz.szymanski1@gmail.com>
2016-09-16 23:33:16 +0200

    Commit do zadań z lekcji 4

--- src/kdkprojekt1_koty/KdkProjekt1_koty.java
+++ src/kdkprojekt1_koty/KdkProjekt1_koty.java
@@ -2,6 +2,8 @@ package kdkprojekt1_koty;
 
 import java.util.Date;
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class KdkProjekt1_koty {
 
@@ -15,16 +17,34 @@ public class KdkProjekt1_koty {
         kot1.setData("2016.01.01");
         System.out.println(kot1.przedstawSie());
         System.out.println("______________________");
+        
         Kot kot2 = new Kot();
         System.out.print("Podaj imię kota: ");
         kot2.setImie(getUserInput());
         System.out.print("Podaj imię opiekuna: ");
         kot2.setImieOpiekuna (getUserInput());
-        System.out.print("Podaj wage: ");
-        kot2.setWaga (Double.valueOf(getUserInput()));
-        System.out.print("Data urodzenia kota: ");
-        kot2.setData(getUserInput());
-        kot2.przedstawSie();
+        
+        Pattern pattern = Pattern.compile("[0-9]+(.[0-9]+)?");
+        Matcher matcher;
+        String tempWaga;
+        do{
+            System.out.print("Podaj wage kota: ");
+            tempWaga = getUserInput();
+            matcher = pattern.matcher(tempWaga);
+        }while (!matcher.matches());
+               
+        kot2.setWaga (Double.valueOf(tempWaga));
+        
+        pattern = Pattern.compile("[0-9]{4}.[0-1]?[0-9].[0-3]?[0-9]");
+        String tempDataUrodzenia; 
+        do{ 
+            System.out.print("Data urodzenia kota w formacie RRRR.MM.DD: ");
+            tempDataUrodzenia =getUserInput();
+            matcher = pattern.matcher(tempDataUrodzenia);
+        }while (!matcher.matches());
+              
+        kot2.setData(tempDataUrodzenia);
+        System.out.println(kot2.przedstawSie());
         
       
         
